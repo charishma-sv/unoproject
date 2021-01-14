b826cfc12e2160c51eb73fb26c498f0077546db1
charishma-sv <itscharishma@gmail.com>
2021-01-13 21:33:04 -0800

    changed log message

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function hello() {
   const x = 5;
   const y = 'x';
   if (x < 0) {
-    console.log('this is develop');
+    console.log('this is feature');
   } else {
     console.log('hi');
   }
