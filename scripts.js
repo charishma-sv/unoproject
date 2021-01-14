9863b98599ad106615dbaf6f11ba55a57d4a79b7
charishma-sv <itscharishma@gmail.com>
2021-01-13 21:51:47 -0800

    both files changed

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function hello() {
   const x = 9;
   const y = 'x';
   if (x < 0) {
-    console.log('this is feature');
+    console.log('this is feature change');
   } else {
     console.log('hi');
   }
