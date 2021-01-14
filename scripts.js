be5ab8bf507e9d9489aae55d9ecdfe1694a397c4
charishma-sv <itscharishma@gmail.com>
2021-01-14 06:23:26 -0800

    made changes in script

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 function hello() {
-  const x = 7;
-  console.log(x);
+  const x = 15;
+  console.log('this is charishma');
   const y = 'x';
   if (x < 0) {
-    console.log('this is feature change');
+    console.log('this is charishma change');
   } else {
     console.log('hi');
   }
